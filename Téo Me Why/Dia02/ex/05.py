db7b128bb25d35b91fd44f203649cfa41f274457
Lucas Vinicyus <46406757+lucasvinicyus@users.noreply.github.com>
2024-12-16 17:47:35 -0300

    Delete Téo Me Why/Dia02/ex/05.py

--- Téo Me Why/Dia02/ex/05.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que exiba o dobro de um numero inserido pelo usuario
-
-numero = input("Entre com um numero: ")
-
-resultado = 2 * numero
-
-print("O dobro de", numero, "é:", resultado)
