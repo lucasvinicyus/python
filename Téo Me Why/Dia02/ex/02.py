a26a5d523e9b3e339f34275e3a62511efa4618f1
Lucas Vinicyus <46406757+lucasvinicyus@users.noreply.github.com>
2024-12-16 17:47:01 -0300

    Delete Téo Me Why/Dia02/ex/02.py

--- Téo Me Why/Dia02/ex/02.py
+++
@@ -1,4 +0,0 @@
-# Faça um programa que de bom dia, pergunte o nome da pessoa e responde que é um prazer conhecer ela, citando o nome da pessoa. 
-nome = input("Bom dia! Qual é o seu nome?\n")
-
-print(nome, "é um prazer te conhecer!")
